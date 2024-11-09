1252d37c64fec069c37f9857d96d72ef202f10a4
Brendanmpeg <112417458+Brendanmpeg@users.noreply.github.com>
2024-11-09 16:10:51 -0500

    Change craftyCrurents to craftingCurrents

--- src/main/java/net/craftingCurrents/craftingCrurentsMod/CraftyCurentsMod.java
+++ src/main/java/net/craftingCurrents/craftingCrurentsMod/CraftyCurentsMod.java
@@ -1,9 +1,7 @@
-package net.craftyCrurents.craftyCrurentsMod;
+package net.craftingCurrents.craftingCrurentsMod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,11 +9,9 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -23,7 +19,7 @@ import org.slf4j.Logger;
 public class CraftyCurentsMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "craftyCrurents";
+    public static final String MOD_ID = "craftingCurrents";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
