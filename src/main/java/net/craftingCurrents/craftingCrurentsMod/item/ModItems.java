c68326e2587e9e42fda70cc3574b8f07ba6168a3
TScholtz <tim.scholtz@outlook.com>
2024-11-09 17:36:04 -0500

    Set up basic code to create blocks and items and add them to the game

--- src/main/java/net/craftingCurrents/craftingCrurentsMod/item/ModItems.java
+++ src/main/java/net/craftingCurrents/craftingCrurentsMod/item/ModItems.java
@@ -1,4 +1,26 @@
 package net.craftingCurrents.craftingCrurentsMod.item;
 
+import net.craftingCurrents.craftingCrurentsMod.CraftingCurentsMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CraftingCurentsMod.MOD_ID);
+
+    //EXAMPLE ITEM
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            () -> new Item(new Item.Properties()));
+    //EXAMPLE ITEM
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
+            () -> new Item(new Item.Properties()));
+
+
+//DO NO DELETE _ METHOD IS USED TO registor items
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
